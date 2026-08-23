f1b8d6a1c02f2843c995f1819acd83bc15fc275b
Miguel Forero <miguelforerodev@gmail.com>
2026-08-23 17:54:04 -0500

    Add React support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 // @ts-check
+import react from "@astrojs/react";
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
@@ -7,4 +8,5 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [react()],
 });
