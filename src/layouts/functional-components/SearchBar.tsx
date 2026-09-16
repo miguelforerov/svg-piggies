6ca4735091002a2e932b2dff8335092a1046de86
Miguel Forero <miguelforerodev@gmail.com>
2026-09-15 20:29:30 -0500

    Use Input store component in Store SearchBar

--- src/layouts/functional-components/SearchBar.tsx
+++ src/layouts/functional-components/SearchBar.tsx
@@ -1,78 +1,36 @@
-import React, { useEffect, useState } from "react";
-import { IoSearch, IoClose } from "react-icons/io5";
+import { useEffect, useState } from "react"
+import SearchInput from "./SearchInput"
 
 const SearchBar = () => {
-  const [isInputEditing, setInputEditing] = useState(false);
-  const [inputValue, setInputValue] = useState("");
+  const [defaultValue, setDefaultValue] = useState("")
 
   useEffect(() => {
-    const searchParams = new URLSearchParams(window.location.search);
-    const query = searchParams.get("q");
-    if (query) {
-      setInputValue(query);
-      setInputEditing(true);
-    }
+    const searchParams = new URLSearchParams(window.location.search)
+    const query = searchParams.get("q")
 
-    const inputField = document.getElementById("searchInput") as HTMLInputElement;
-    if (isInputEditing || query) {
-      inputField.focus();
+    if (query) {
+      setDefaultValue(query)
     }
-  }, [isInputEditing]);
+  }, [])
 
   const updateURL = (query: string) => {
-    const newURL = query ? `/products?q=${encodeURIComponent(query)}` : '/products';
-    // window.history.pushState({}, '', newURL);
-    window.location.href = newURL.toString();
-  };
+    const newURL = query
+      ? `/products?q=${encodeURIComponent(query)}`
+      : "/products"
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputEditing(true);
-    setInputValue(e.target.value);
-
-    updateURL(e.target.value);
-  };
-
-  const handleClear = () => {
-    setInputValue("");
-    setInputEditing(false);
-    updateURL("");
-  };
-
-  const onSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const form = e.target as HTMLFormElement;
-    const searchInput = form.search as HTMLInputElement;
-    updateURL(searchInput.value);
-  };
+    window.location.href = newURL
+  }
 
   return (
-    <form onSubmit={onSubmit} className="rounded-lg flex bg-light dark:bg-dark/10 pl-4 relative">
-      <input
-        type="text"
-        name="search"
-        placeholder="Search for products"
-        autoComplete="off"
-        value={inputValue}
-        onChange={handleChange}
-        id="searchInput"
-        className="bg-transparent border-none search-input focus:ring-transparent p-2 w-full"
-      />
-      <div className="absolute right-0 top-0 flex h-full items-center">
-        {inputValue && (
-          <button
-            type="button"
-            onClick={handleClear}
-            className="p-2 m-1 rounded-full"
-          >
-            <IoClose className="h-4 w-4" />
-          </button>
-        )}
-        <button type="submit" className="search-icon p-2 m-1 rounded-full">
-          <IoSearch className="h-5 w-5" />
-        </button>
-      </div>
-    </form>
-  );
-};
+    <SearchInput
+      name="search"
+      id="searchInput"
+      placeholder="Search for products"
+      autoComplete="off"
+      defaultValue={defaultValue}
+      onSearch={updateURL}
+    />
+  )
+}
 
-export default SearchBar;
+export default SearchBar
